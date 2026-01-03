7ab905e61299f246e10deb158a7edcbe1e8095f5
Lf-PierreC <lfpierre.carvalho@gmail.com>
2026-01-03 14:22:08 -0300

    v.01

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import './Header.css'
 function Header() {
     return (
     <div className='header'>
-        <h1>Luiz & Camilla</h1>
-        <p>(Imagens Meramente Ilustrativas)</p>
+        <img src="../../images/monografia luiz e camilla preto.png" alt="Logo Luiz & Camilla" id="logo"/>
+        <h1>LUIZ & CAMILLA</h1>
     </div>
     )
 }
